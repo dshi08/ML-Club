448e9583d635b48c69e9ec0ae960b5533c22651d
David S <dshi2008mayo@gmail.com>
2025-07-18 21:03:45 -0400

    mobile fixed
    wow who knew deleting centers would make it more centered

--- yo-gurt/src/components/NavBar.js
+++ yo-gurt/src/components/NavBar.js
@@ -26,7 +26,7 @@ function NavBar() {
      <nav className = "navbar">
         <div className = "navbar-container">
             <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
-                AHS ML <video src='/videos/trimmed.mov' className="navbar-icon" autoPlay loop muted playsInline/>
+                AHSMLC <video src='/videos/trimmed.mov' className="navbar-icon" autoPlay loop muted playsInline/>
             </Link>
             <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
