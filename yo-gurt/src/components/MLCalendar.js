51a8d4831b97e1a8edaff6a03e2d666bee285afb
David S <dshi2008mayo@gmail.com>
2025-07-20 16:08:13 -0400

    calendar but probably going to use google calendar api instead
    - made calendar class and css added

---
+++ yo-gurt/src/components/MLCalendar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './MLCalendar.css'
+import '../App.css'
+
+
+function Calendar() {
+  return (
+    <div className='calendar-container'>
+      <div className='calendar-content'>
+        <iframe 
+          src="https://calendar.google.com/calendar/embed?src=dshi2008mayo%40gmail.com&ctz=America%2FNew_York"
+          style={{ border: 0 }}
+          width="800"
+          height="600"
+          frameBorder="0"
+          scrolling="no"
+          title="ML Club"
+        ></iframe>
+      </div>
+    </div>
+  )
+}
+
+export default Calendar
