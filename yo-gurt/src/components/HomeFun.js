8a54083bde4b77c54ff05ebd6463a15f396b7ed9
David S <dshi2008mayo@gmail.com>
2025-07-22 21:25:42 -0400

    changed home fun to become meeting leaders

--- yo-gurt/src/components/HomeFun.js
+++ yo-gurt/src/components/HomeFun.js
@@ -6,22 +6,30 @@ function HomeFun() {
 
   return (
     <div className = 'home-fun-cards'>
-        <h1>Have Fun,</h1>
-        <h2>Make Friends!</h2>
+        <h1>Meet our board</h1>
+        <h2>Seeking new leaders</h2>
         <div className='cards__container'>
             <div className='cards__wrapper'>
                 <ul className='cards__items'>
                     <CardItem
                     src='/images/banana.png' 
-                    label='Have'
-                    text='Machine 🍌🍌🍌'
+                    label='Club President'
+                    text="I'm David, a senior at AHS. My favorite past times include doomscrolling and bedrotting! "
+                    path='https://www.youtube.com/watch?v=xPaFQ8ogvto'
+                    />
+                </ul>
+                <ul className='cards__items'>
+                    <CardItem
+                    src='/videos/trimmed.gif' 
+                    label='Vice President'
+                    text='SEEKING VP'
                     path='https://www.youtube.com/watch?v=xPaFQ8ogvto'
                     />
                     <CardItem
-                    src='/images/cubesat.png' 
-                    label='Fun!'
-                    text='Team working on BWSI Build a CubeSat Challenge'
-                    path='https://drive.google.com/drive/folders/1Oa1vfpYRJ8C8R1q91U7ICeeJE4_YPFjU'
+                    src='/videos/trimmed.gif' 
+                    label='Social Media Manager'
+                    text='SEEKING SOCIAL MEDIA MANAGER'
+                    path='https://www.youtube.com/watch?v=xPaFQ8ogvto'
                     />
                 </ul>
             </div>
