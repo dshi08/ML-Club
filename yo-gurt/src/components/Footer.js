78be212b4ead4658a181899ebcd0a681e5f96932
David S <dshi2008mayo@gmail.com>
2025-07-22 23:05:32 -0400

    foooterrrrs and changed card font
    - also changed home page so the projects background blends with next background
    - changed plant card image cause res too low

--- yo-gurt/src/components/Footer.js
+++ yo-gurt/src/components/Footer.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import './Footer.css'
 
 function Footer() {
   return (
     <div className='footer-container'>
-        
+        <div className='footer-links'>
+          <div className='footer-link-wrapper'>
+            <div className='footer-link-items'>
+              <h2>Contacts</h2>
+              <p>David Shi(President): <a href="mailto:dshi2026@student.andoverma.us">dshi2026@student.andoverma.us</a></p>
+              <p>Will Lau(Vice President): <a href="mailto:wlau2026@student.andoverma.us">wlau2026@student.andoverma.us</a></p>
+              <p>Dylan Ho(Socials Manager): <a href="mailto:dho2026@student.andoverma.us">dho2026@student.andoverma.us</a></p>
+              <p>Dr. Bezaire(club Advisor): <a href="mailto:marianne.bezaire@andoverma.us">marianne.bezaire@andoverma.us</a></p>
+            </div>
+          </div>
+        </div>
+        <section className='social-media'>
+          <div className='social-media-wrap'>
+            <div className='footer-logo'>
+              <a className='social-logo'
+                  href='https://www.instagram.com/ahs.mlclub/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+              >
+                <i className='fab fa-instagram'/>
+              </a>
+            </div>
+          </div>
+        </section>
     </div>
   )
 }
--- yo-gurt/src/components/pages/Home.js
+++ yo-gurt/src/components/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Hero'
 import HomeProject from '../HomeProject'
 import HomeFun from '../HomeFun'
+import Footer from '../Footer'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Hero/>
       <HomeProject/>
       <HomeFun/>
+      <Footer/>
     </>
   )
 }
